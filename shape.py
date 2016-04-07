9d36f114227b178228eb4a88ba0a7c9a1eab0cb0
Pib <alexis.pibrac@gmail.com>
2016-04-07 10:52:13 +0800

    in shape.py: add multiple f / in 01: implement benefit and rating f

--- shape.py
+++ shape.py
@@ -11,6 +11,19 @@ from math import sqrt
 import matplotlib.patches as patch
 
 
+def multiple_entry(double_entry):
+    """transform a function(a1,a2) in function(a1,a2...an)"""
+    def outf(*args):
+        out=0
+        p0=args[0]
+        for p in args:
+            if p is p0:
+                continue
+            out+=double_entry(p0,p)
+            p0=p
+        return out
+    return outf
+
 
 
 #Basic envelopes
@@ -24,13 +37,14 @@ class Network:
     gpatch - > the patch of the network to be drawn, see matplotlib.patches
     Xsize - > (xmin,xmax) minimum and maximum of the network in X axe
     Ysize - > same for Y"""
-    def __init__(self,position_generator,travel_time,travel_distance,gpatch=None,Xsize=None,Ysize=None):
+    def __init__(self,position_generator,travel_distance,travel_time,distance_travelled,gpatch=None,Xsize=None,Ysize=None):
         self.position_generator=position_generator
-        self.travel_time=travel_time
         self.get_patch=gpatch
-        self.travel_distance=travel_distance
+        self.distance_travelled=distance_travelled
         self.Xsize=Xsize
         self.Ysize=Ysize
+        self.travel_time=multiple_entry(travel_time)
+        self.travel_distance=multiple_entry(travel_distance)
         
         
 class Timer:
@@ -57,15 +71,17 @@ def rectangle(X,Y,basic_speed):
         x=X[0]+random()*(X[1]-X[0])
         y=Y[0]+random()*(Y[1]-Y[0])
         return (x,y)
-    def travel(p1,p2):
+    def travel_d(p1,p2):
         dist=(p1[0]-p2[0])**2+(p1[1]-p2[1])**2
         dist=sqrt(dist)
-        return dist/basic_speed
+        return dist
+    def travel_t(p1,p2):
+        return travel_d(p1,p2)/basic_speed
     def dist_travelled(t):
         return t*basic_speed
     def gpatch():
         return patch.Rectangle((X[0],Y[0]),X[1]-X[0], Y[1]-Y[0])
-    return Network(position,travel,dist_travelled,gpatch,X,Y)
+    return Network(position,travel_d,travel_t,dist_travelled,gpatch,X,Y)
 
 def square(a,basic_speed):
     """square of size a"""
@@ -81,15 +97,17 @@ def circle(R, basic_speed):
             x=S[0]+random()*(S[1]-S[0])
             y=S[0]+random()*(S[1]-S[0])
         return (x,y)
-    def travel(p1,p2):
+    def travel_d(p1,p2):
         dist=(p1[0]-p2[0])**2+(p1[1]-p2[1])**2
         dist=sqrt(dist)
-        return dist/basic_speed
+        return dist
+    def travel_t(p1,p2):
+        return travel_d(p1,p2)/basic_speed
     def dist_travelled(t):
         return t*basic_speed
     def gpatch():
         return patch.Circle((0,0),R)
-    return Network(position,travel,dist_travelled,gpatch,S,S)
+    return Network(position,travel_d,travel_t,dist_travelled,gpatch,S,S)
     
 
 
